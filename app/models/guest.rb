f3c6a9e9bf415a62e6432b5b5353170252e76e06
ipc103 <iancandysemail@gmail.com>
2017-04-18 10:50:18 -0400

    add top guests

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,12 @@
 class Guest < ApplicationRecord
   has_many :appearances
   has_many :episodes, through: :appearances
+
+  def self.top_guests
+    joins(:appearances).group('guests.id').order('AVG(rating) DESC')
+  end
+
+  def average_rating
+    appearances.average(:rating).to_f
+  end
 end
