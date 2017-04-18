01facfe7ebdb5beeceffc578a82f82d05bc13ecf
ipc103 <iancandysemail@gmail.com>
2017-04-18 10:42:09 -0400

    add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :logged_in?
+
+  private
+
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  def authorize_user
+    unless logged_in?
+      flash[:notice] = 'You must be logged in to view this page'
+      redirect_to login_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'guests#index'
+
   resources :guests, only: [:index, :show]
   resources :episodes, only: [:index, :show]
 
@@ -7,4 +9,5 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/sessions', to: 'sessions#create', as: 'sessions'
+  delete '/sessions', to: 'sessions#destroy', as: 'logout'
 end
